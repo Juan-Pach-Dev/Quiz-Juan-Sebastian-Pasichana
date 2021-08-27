72c422589093f5f47fab5657a02e2bbee85fa69f
Juan-Pach-Dev <jusepamo01@hotmail.com>
2021-08-27 16:18:44 -0500

    Agregar archivos de proyecto.

---
+++ Quiz Juan Sebastian Pasichana/FormularioQuiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Juan_Sebastian_Pasichana
+{
+    public partial class FormularioQuiz : Form
+    {
+        public FormularioQuiz()
+        {
+            InitializeComponent();
+        }
+    }
+}
